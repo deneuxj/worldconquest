e655905e5565122534a702734594d7211bd98719
Johann Deneux <Johann Deneux@localhost>
2010-12-22 19:02:36 +0100

    Added script experimenting with XNA and WinForms.

---
+++ WorldConquest/XnaWinForm.fsx
@@ -0,0 +1,47 @@
+﻿#r @"D:\Documents\WinForms\WinFormsGraphicsDevice\bin\Release\WinFormsGraphicsDevice.exe"
+
+#I @"C:\Program Files\Microsoft XNA\XNA Game Studio\v4.0\References\Windows\x86"
+#r "Microsoft.Xna.Framework.Graphics.dll"
+#r "Microsoft.Xna.Framework.dll"
+#r "Microsoft.Xna.Framework.Game.dll"
+
+open System.Windows.Forms
+open Microsoft.Xna.Framework
+
+type XnaControl() =
+    inherit WinFormsGraphicsDevice.GraphicsDeviceControl()
+
+    let mutable drawer = fun (dt : GameTime) -> ()
+    let watch = new System.Diagnostics.Stopwatch()
+    let mutable last_time = watch.Elapsed
+
+    member this.Drawer
+        with get ()  = drawer
+        and  set (v) = drawer <- v
+        
+    override this.Initialize() =
+        watch.Start()
+        last_time <- watch.Elapsed
+
+    override this.Draw() =
+        let diff = watch.Elapsed - last_time
+        last_time <- watch.Elapsed
+        GameTime(diff, watch.Elapsed)
+        |> drawer
+
+type XnaForm() =
+    inherit Form()
+
+    let ctrl = new XnaControl()
+    let animationHandler = new System.EventHandler(fun _ _ -> ctrl.Invalidate())
+    do
+        ctrl.Dock <- DockStyle.Fill
+        base.Controls.Add(ctrl)
+
+    member this.XnaControl = ctrl
+
+    member this.EnableAnimation() =
+        Application.Idle.AddHandler(animationHandler)
+
+    member this.DisableAnimation() =
+        Application.Idle.RemoveHandler(animationHandler)
