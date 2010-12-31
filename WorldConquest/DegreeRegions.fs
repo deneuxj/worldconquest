6cc70198dca7b3e9b7e76da1efeac0d7fa4d275c
Johann Deneux <Johann Deneux@localhost>
2010-12-31 16:21:32 +0100

    Started work on terrain preprocessing which might be useful for AI. Note: might not be useful at all, I am dropping the idea for now.

---
+++ WorldConquest/DegreeRegions.fs
@@ -0,0 +1,43 @@
+﻿module DegreeRegions
+
+open Terrain
+open HexTiling
+
+let markRegions (terr : Terrain[,]) =
+    let width = Array2D.length1 terr
+    let height = Array2D.length2 terr
+
+    let nbrsOf (i, j) =
+        (i, j)
+        |> SquareCoords
+        |> neighboursOfSq
+        |> List.filter (fun (SquareCoords(i, j)) -> j >= 0 && j < height)
+        |> List.map (fun (SquareCoords(i, j)) -> (if i < 0 then width + i elif i >= width then i - width else i), j)
+
+    let land_ratios =
+        terr
+        |> Array2D.mapi (fun i j _ ->
+            let nbrs = nbrsOf(i, j)
+            let n_sea =
+                nbrs
+                |> Seq.filter (fun (i,j) -> terr.[i,j] = Sea)
+                |> Seq.length
+            let n_land =
+                nbrs
+                |> Seq.filter (fun (i, j) -> terr.[i,j] = Land)
+                |> Seq.length
+            (float32 n_sea) / (float32 n_land)
+        )
+
+    let regions = terr |> Array2D.map (fun _ -> -1)
+
+    // TODO
+    let paintFill c0 id =
+        let mutable working = [ c0 ]
+        while
+            match working with
+            | [] -> false
+            | _ -> true
+            do
+            ()                
+    regions
--- WorldConquest/HexTiling.fs
+++ WorldConquest/HexTiling.fs
@@ -21,3 +21,40 @@ let dist (HexCoords(x0, y0) as c0) (HexCoords(x1, y1) as c1) : int =
         max dx -dy
     else
         abs(dx+dy)
+
+let neighboursOfSq (SquareCoords(i, j)) =
+    let i0 = i-1
+    let i1 = i
+    let i2 = i+1
+    let j0 = j-1
+    let j1 = j
+    let j2 = j+1
+
+    if j % 2 = 0 then
+        [
+             (i0, j0); (i1, j0);
+          (i0, j1);       (i2, j1);
+             (i0, j2); (i1, j2)
+        ]
+    else
+        [
+             (i1, j0); (i2, j0);
+          (i0, j1);       (i2, j1);
+             (i1, j2); (i2, j2)
+        ]
+    |> List.map SquareCoords
+
+let neighboursOf (HexCoords(i, j)) =
+    let i0 = i-1
+    let i1 = i
+    let i2 = i+1
+    let j0 = j-1
+    let j1 = j
+    let j2 = j+1
+
+    [
+            (i1, j0); (i2, j0);
+        (i0, j1);       (i2, j1);
+            (i0, j2); (i1, j2)
+    ]
+    |> List.map HexCoords
