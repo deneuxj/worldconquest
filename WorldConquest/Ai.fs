4d24fddcd42b2444c226c52e09d9b469f74d3384
johdex <johdex@localhost>
2011-04-17 00:00:15 +0200

    Moved some code to GameState.fs

--- WorldConquest/Ai.fs
+++ WorldConquest/Ai.fs
@@ -7,56 +7,19 @@ open Units
 
 let getValidOrders (state : GameState) (player : int) =
     let width = state.terrain.GetLength(0)
-    let getUnitOrders = getOrder state player
-    [|
-        let getUnitOrders u =
-            let destinations =
-                getAllWithin (getMovementRange u.specific)
-                |> Array.filter (fun (HexCoords(_, y)) -> y >= 0 && y < width)
-                |> Array.map (fromHex >> wrapX width >> toHex)
-            destinations
-            |> Array.map (getUnitOrders u)
-            |> List.concat
+    let getUnitOrders =
+        getOrder state player
 
-        for u in state.player_units.[player] do
-            yield! getUnitOrders u
+    let getUnitOrders u =
+        let destinations =
+            getAllWithin (getMovementRange u.specific)
+            |> Array.filter (fun (HexCoords(_, y)) -> y >= 0 && y < width)
+            |> Array.map (fromHex >> wrapX width >> toHex)
+        destinations
+        |> Array.map (getUnitOrders u >> Array.ofList)
+        |> Array.concat
 
-            match u.specific with
-            | Transport(_, transported) ->
-                for t in transported do
-                    let unit_type =
-                        match t with
-                        | TransportedUnit.Infantry _ -> Infantry
-                        | TransportedUnit.Tank _ -> Tank
-                        | TransportedUnit.Artillery _ -> Artillery
-                        | TransportedUnit.AntiAircraft _ -> AntiAircraft
-                    let u' =
-                        {  coords = u.coords;
-                           health = t.Health;
-                           moves = getMovementRange unit_type;
-                           specific = unit_type  }
-                    yield! getUnitOrders u' |> List.filter (function Move _ -> true | _ -> false)
-            | Bomber(_, _, BomberTransport.Infantry(Health h)) ->
-                let u' =
-                    {  coords = u.coords;
-                       health = h;
-                       moves = getMovementRange Infantry
-                       specific = Infantry  }
-                yield! getUnitOrders u'
-            | Carrier(_, aircrafts) ->
-                for plane in aircrafts do
-                    let unit_type =
-                        match plane with
-                        | CarriedAircraft.Fighter _ -> Fighter(Landed, Fuel fighter_fuel_range)
-                        | CarriedAircraft.Bomber (transported, _) -> Bomber(Landed, Fuel bomber_fuel_range, transported)
-                    let u' =
-                        {  coords = u.coords;
-                           health = plane.Health;
-                           moves = getMovementRange unit_type;
-                           specific = unit_type  }
-                    yield! getUnitOrders u'
-            | _ -> ()
-    |]
+    playerUnitMap getUnitOrders (function Move _ -> true | _ -> false) state.player_units.[player]
 
 // Position evaluation functions
 
