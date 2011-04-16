a291f850313246312ea72f9dd785155c562dcb70
johdex <johdex@localhost>
2011-04-16 23:59:28 +0200

    More work on executing orders.

--- WorldConquest/GameState.fs
+++ WorldConquest/GameState.fs
@@ -108,9 +108,9 @@ let getOrder (gs : GameState) (player : int) =
                             if i <> player then
                                 yield! gs.player_units.[i] |> Array.filter (fun u -> u.coords = destination)
                     }
-                   |> Seq.exists (fun target -> canBom(unit, target))
+                   |> Seq.exists (fun target -> canBomb(unit, target))
             then
-                canMove dist (unit.moves)
+                canMove (dist destination) (unit.moves)
             else
                 None
 
@@ -266,6 +266,55 @@ let getOrder (gs : GameState) (player : int) =
             | None -> ()
         ]
 
+// f maps a unit to a sequence of some generic data
+// g filters the generic data for units that are inside a transport.
+// g is needed to e.g. prevent artilleries to fire from transports.
+let playerUnitMap (f : UnitInfo -> 'T[]) g (units : UnitInfo seq) =
+    [|
+        for u in units do
+            yield f u
+
+            match u.specific with
+            | Transport(_, transported) ->
+                for t in transported do
+                    let unit_type =
+                        match t with
+                        | TransportedUnit.Infantry _ -> Infantry
+                        | TransportedUnit.Tank _ -> Tank
+                        | TransportedUnit.Artillery _ -> Artillery
+                        | TransportedUnit.AntiAircraft _ -> AntiAircraft
+                    let u' =
+                        {  coords = u.coords;
+                           health = t.Health;
+                           moves = getMovementRange unit_type;
+                           specific = unit_type  }
+                    yield f u' |> Array.filter g
+            | Bomber(_, _, BomberTransport.Infantry(Health h)) ->
+                let u' =
+                    {  coords = u.coords;
+                       health = h;
+                       moves = getMovementRange Infantry
+                       specific = Infantry  }
+                yield f u'
+            | Carrier(_, aircrafts) ->
+                for plane in aircrafts do
+                    let unit_type =
+                        match plane with
+                        | CarriedAircraft.Fighter _ -> Fighter(Landed, Fuel fighter_fuel_range)
+                        | CarriedAircraft.Bomber (transported, _) -> Bomber(Landed, Fuel bomber_fuel_range, transported)
+                    let u' =
+                        {  coords = u.coords;
+                           health = plane.Health;
+                           moves = getMovementRange unit_type;
+                           specific = unit_type  }
+                    yield f u'
+            | _ -> ()
+    |]
+
+type UnitTempState =
+    | Embarking of UnitInfo * int
+    | Killed of UnitInfo
+    | Normal of UnitInfo
 
 let executeOrders (gs : GameState) (player : int) (orders : Order[]) =
     let width = gs.terrain.GetLength(0)
@@ -275,9 +324,35 @@ let executeOrders (gs : GameState) (player : int) (orders : Order[]) =
         | path -> path |> List.rev |> List.head
 
         match order with
-        | Move [] | DirectAttack [] | Conquer [] -> u
-        | Move path | DirectAttack path | Conquer path ->
-            { u with coords = getDestination path }
+        | Move path | DirectAttack path | Conquer path | DockAt path | LandAt path ->
+            Normal { u with coords = getDestination path }
+        | Bomb path ->
+            match u.specific with
+            | Bomber(Airborne, fuel, BomberTransport.Bombs n) ->
+                Normal { u with coords = getDestination path;
+                                specific = Bomber(Airborne, fuel, BomberTransport.Bombs (n - 1)) }
+            | _ -> failwith "Only airborne bombers carrying bombs may bomb"
+        | Bombard _ ->
+            Normal u
+        | DoNothing ->
+            Normal u
+        | Load (path, idx_transport) ->
+            Embarking ({ u with coords = getDestination path }, idx_transport)
+        | Unload (path, _) ->
+            match u.specific with
+            | Transport(docked, _) ->
+                Normal { u with coords = getDestination path;
+                                specific = Transport(docked, []) }
+            | Bomber(landed, fuel, _) ->
+                Normal { u with coords = getDestination path;
+                                specific = Bomber(landed, fuel, BomberTransport.Bombs 0) }
+            | _ -> failwith "Only transports and bombers can unload their transported units"
 
-    ()
+    let it = (orders :> System.Collections.Generic.IEnumerable<Order>).GetEnumerator()
+    let fetchAndExecuteOrder u =
+        let order = it.MoveNext() |> fun _ -> it.Current
+        [| executeOrder (u, order) |]
+
+    playerUnitMap fetchAndExecuteOrder (fun _ -> true) gs.player_units.[player]
+    |> Array.concat
     
