aed7e7d2e79a7a38b9328175e869f4214f23326d
johdex <johdex@localhost>
2011-05-21 18:07:11 +0200

    Removed tests for capturing.

--- PexTests.Generated/GameStateUpdateTestsGenerated.TestFuelDecreases.g.cs
+++ PexTests.Generated/GameStateUpdateTestsGenerated.TestFuelDecreases.g.cs
@@ -16,49 +16,5 @@ using Microsoft.ExtendedReflection.DataAccess;
 namespace WorldConquest.PexTests
 {
   public partial class GameStateUpdateTestsGenerated {
-[TestMethod]
-[PexGeneratedBy(typeof(GameStateUpdateTests))]
-[Ignore]
-[PexDescription("the test state was: path bounds exceeded")]
-public void TestFuelDecreases967()
-{
-    int[][] intss = new int[2][];
-    int[] ints = new int[2];
-    intss[0] = ints;
-    int[] ints1 = new int[2];
-    intss[1] = ints1;
-    ((GameStateUpdateTests)this).TestFuelDecreases(intss, false, 
-                                                          global::Units.BomberTransport.NewInfantry((global::Units.Health)null));
-}
-[TestMethod]
-[PexGeneratedBy(typeof(GameStateUpdateTests))]
-[Ignore]
-[PexDescription("the test state was: path bounds exceeded")]
-public void TestFuelDecreases322()
-{
-    int[][] intss = new int[2][];
-    int[] ints = new int[2];
-    intss[0] = ints;
-    int[] ints1 = new int[2];
-    intss[1] = ints1;
-    ((GameStateUpdateTests)this).TestFuelDecreases
-        (intss, PexSafeHelpers.ByteToBoolean((byte)64), 
-                global::Units.BomberTransport.NewInfantry((global::Units.Health)null));
-}
-[TestMethod]
-[PexGeneratedBy(typeof(GameStateUpdateTests))]
-[Ignore]
-[PexDescription("the test state was: path bounds exceeded")]
-public void TestFuelDecreases01()
-{
-    int[][] intss = new int[2][];
-    int[] ints = new int[2];
-    intss[0] = ints;
-    int[] ints1 = new int[2];
-    intss[1] = ints1;
-    ((GameStateUpdateTests)this).TestFuelDecreases
-        (intss, PexSafeHelpers.ByteToBoolean((byte)16), 
-                global::Units.BomberTransport.NewBombs(0));
-}
   }
 }
--- PexTests.Generated/GameStateUpdateTestsGenerated.TestGrowUnitTree.g.cs
+++ PexTests.Generated/GameStateUpdateTestsGenerated.TestGrowUnitTree.g.cs
@@ -16,51 +16,5 @@ using Microsoft.Pex.Framework.Generated;
 namespace WorldConquest.PexTests
 {
   public partial class GameStateUpdateTestsGenerated {
-[TestMethod]
-[PexGeneratedBy(typeof(GameStateUpdateTests))]
-public void TestGrowUnitTree423()
-{
-    global::EmbarkOrders.EmbarkOrder embarkOrder;
-    Tuple<FSharpOption<global::GameState.UnitIndex>, global::Units.UnitInfo>[] 
-      tuples;
-    embarkOrder = global::EmbarkOrdersFactory.CreateEmbarkOrder
-                      (0, global::GameState.UnitIndex.NewTransported2(0, 0, 0));
-    global::EmbarkOrders.EmbarkOrder[] embarkOrders
-       = new global::EmbarkOrders.EmbarkOrder[1];
-    global::EmbarkOrders.DisembarkOrder[] disembarkOrders
-       = new global::EmbarkOrders.DisembarkOrder[0];
-    global::Units.UnitInfo[] unitInfos = new global::Units.UnitInfo[0];
-    embarkOrders[0] = embarkOrder;
-    tuples = ((GameStateUpdateTests)this).TestGrowUnitTree
-                 (embarkOrders, disembarkOrders, unitInfos);
-    Assert.IsNotNull((object)tuples);
-    Assert.AreEqual<int>(0, tuples.Length);
-}
-[TestMethod]
-[PexGeneratedBy(typeof(GameStateUpdateTests))]
-[PexRaisedException(typeof(Exception))]
-public void TestGrowUnitTreeThrowsException430()
-{
-    global::EmbarkOrders.EmbarkOrder embarkOrder;
-    global::Units.UnitInfo unitInfo;
-    Tuple<FSharpOption<global::GameState.UnitIndex>, global::Units.UnitInfo>[] 
-      tuples;
-    embarkOrder = global::EmbarkOrdersFactory.CreateEmbarkOrder
-                      (0, global::GameState.UnitIndex.NewTransported2(0, 0, 0));
-    unitInfo = global::UnitsFactory.CreateUnitInfo
-                   ((global::HexTiling.HexCoords)null, (float)0, 0, false, 
-                    0, (global::Units.BomberTransport)null, 
-                    (global::Units.CarriedAircraft[])null, 
-                    (global::Units.TransportedUnit[])null);
-    global::EmbarkOrders.EmbarkOrder[] embarkOrders
-       = new global::EmbarkOrders.EmbarkOrder[1];
-    global::EmbarkOrders.DisembarkOrder[] disembarkOrders
-       = new global::EmbarkOrders.DisembarkOrder[0];
-    global::Units.UnitInfo[] unitInfos = new global::Units.UnitInfo[1];
-    embarkOrders[0] = embarkOrder;
-    unitInfos[0] = unitInfo;
-    tuples = ((GameStateUpdateTests)this).TestGrowUnitTree
-                 (embarkOrders, disembarkOrders, unitInfos);
-}
   }
 }
