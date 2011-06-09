2e56c3cb633310165cabf6776df7ab8e668fa772
Johann Deneux <johann.deneux@gmail.com>
2011-06-09 06:38:45 +0200

    Started work on production.

--- WorldConquest/Resource.fs
+++ WorldConquest/Resource.fs
@@ -1,10 +1,26 @@
 ﻿module Resource
 
+type Production =
+    | Infantry
+    | Tank
+    | Artillery
+    | AntiAircraft
+    | Transport
+    | Destroyer
+    | Submarine
+    | Battleship
+    | Carrier
+    | Fighter
+    | Bomber
+
+type ProductionState =
+    { prod : Production
+      turns_left : int }
+
 type Resource =
     | Oil
     | Wood
     | Iron
-    | Factory
+    | Factory of ProductionState option
     | Harbour
     | Airfield
-
